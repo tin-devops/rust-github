64ed30bbfd30ca6add59f7f347d117a0bd334a93
github-action-benchmark <github@users.noreply.github.com>
2022-10-30 16:54:38 +0000

    add Rust Benchmark (cargo) benchmark result for d654f229356ed707621f95526e843821ba8d20ce

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1667147958898,
+  "lastUpdate": 1667148878161,
   "repoUrl": "https://github.com/tin-devops/rust-github",
   "entries": {
     "Rust Benchmark": [
@@ -23,6 +23,66 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/tin-devops/rust-github/commit/d654f229356ed707621f95526e843821ba8d20ce"
         },
         "date": 1667147958568,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "BenchFib10",
+            "value": 137,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "BenchFib20",
+            "value": 20465,
+            "range": "± 952",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/20",
+            "value": 20524,
+            "range": "± 1161",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/20",
+            "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/21",
+            "value": 30594,
+            "range": "± 5193",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/21",
+            "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tindevops@gmail.com",
+            "name": "tindevops",
+            "username": "DevOpsSenior"
+          },
+          "committer": {
+            "email": "tindevops@gmail.com",
+            "name": "tindevops",
+            "username": "DevOpsSenior"
+          },
+          "distinct": true,
+          "id": "d654f229356ed707621f95526e843821ba8d20ce",
+          "message": "first commit",
+          "timestamp": "2022-10-30T23:38:50+07:00",
+          "tree_id": "a19806dfdaab588dbceccfac43a7ff764487c87b",
+          "url": "https://github.com/tin-devops/rust-github/commit/d654f229356ed707621f95526e843821ba8d20ce"
+        },
+        "date": 1667148877461,
         "tool": "cargo",
         "benches": [
           {
